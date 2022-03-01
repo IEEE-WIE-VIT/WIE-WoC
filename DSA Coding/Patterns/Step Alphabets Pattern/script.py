e3cc2d186fdaa50822428e2deb3e031dda939410
Harsha <77226384+saiharshavarma@users.noreply.github.com>
2022-03-02 00:01:34 +0530

    Step Alphabets Pattern

---
+++ DSA Coding/Patterns/Step Alphabets Pattern/script.py
@@ -0,0 +1,10 @@
+Height = int(input("Enter the height of the pattern : "))
+if Height < 0:
+    print("Invalid Input")
+elif Height == 0:
+    print()
+else:
+    for row_counter in range(1,Height+1):
+        for column_counter in range(1,row_counter+1):
+            print(chr(96 + column_counter), end=" ")
+        print()
